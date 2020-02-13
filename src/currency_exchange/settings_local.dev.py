55bcfb6a37bb0e994b779e33c28c61d186d57b49
ZenCrazyCat <zencrazycat@gmail.com>
2020-02-13 20:30:42 +0200

    setup project

---
+++ src/currency_exchange/settings_local.dev.py
@@ -0,0 +1,7 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'qkngi6q!7y74@lht)x2ivvzcd%^s@g0@_j$^78%$#nft828#dj'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
