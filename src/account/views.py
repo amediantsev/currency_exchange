a7ab82d09caf30269941eea9299a30ed07f2443d
ZenCrazyCat <zencrazycat@gmail.com>
2020-02-18 22:35:23 +0200

    Home work number 16

--- src/account/views.py
+++ src/account/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from account.forms import UserCreationFormReboot
 from django.urls import reverse_lazy
 from django.views import generic
 
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormReboot
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
