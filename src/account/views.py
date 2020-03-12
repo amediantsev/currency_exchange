0b123d690a6a7ed079ba128dbd5e0bb67d1d5534
ZenCrazyCat <zencrazycat@gmail.com>
2020-03-12 12:38:50 +0200

    redo table on dark theme

--- src/account/views.py
+++ src/account/views.py
@@ -22,7 +22,7 @@ class MyProfile(generic.UpdateView):
     # def get_context_data(self, *args,  **kwargs):
     #     context = super().get_context_data(**kwargs)
     #     context['pk'] = self.request.user.id
-    #     context['current_user'] = int(self.request.path[-2])
+    #     context['current_user'] = int(self.request.path[2])
     #     return context
 
     # def form_valid(self, form):
