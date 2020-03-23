82119da63088767d0bd6e9c99fda2557a75fa220
ZenCrazyCat <zencrazycat@gmail.com>
2020-03-23 14:46:31 +0200

    fix _privat

--- src/currency/models.py
+++ src/currency/models.py
@@ -10,6 +10,9 @@ class Rate(models.Model):
     sale = models.DecimalField(max_digits=4, decimal_places=2)
     source = models.PositiveSmallIntegerField(choices=mch.SOURCE_CHOICES)
 
+    class Meta():
+        ordering = ('created', )
+
     def __str__(self):
         return f'{self.get_source_display()} ' \
                f'{self.created.year}-{self.created.month}-{self.created.day} ' \
