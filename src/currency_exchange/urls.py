95d1c9596f3d8b09650056b5ba9e6356f79df051
ZenCrazyCat <zencrazycat@gmail.com>
2020-03-30 23:26:11 +0300

    add debug-toolbar

--- src/currency_exchange/urls.py
+++ src/currency_exchange/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
 import django.contrib.auth.urls
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+import debug_toolbar
 
 
 urlpatterns = [
@@ -21,3 +21,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
