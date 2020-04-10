887a444ccc9eecddbf4d4112c74578faa1e32f12
ZenCrazyCat <zencrazycat@gmail.com>
2020-04-10 13:11:24 +0300

    class work with updates

--- src/currency/models.py
+++ src/currency/models.py
@@ -1,7 +1,10 @@
+from django.core.cache import cache
 from django.db import models
 from django.utils import timezone
 
 from currency import model_choices as mch
+from currency.utils import generate_rate_cache_key
+
 
 class Rate(models.Model):
     created = models.DateTimeField(default=timezone.now)
@@ -18,3 +21,11 @@ class Rate(models.Model):
                f'{self.created.year}-{self.created.month}-{self.created.day} ' \
                f'{self.created.hour}:{self.created.minute} ' \
                f'{self.get_currency_display()} {self.buy} {self.sale}'
+
+    def save(self, *args, **kwargs):
+
+        if not self.id:
+            cache_key = generate_rate_cache_key(self.source, self.currency)
+            cache.delete(cache_key)
+
+        super().save(*args, **kwargs)
--- src/currency/urls.py
+++ src/currency/urls.py
@@ -8,4 +8,5 @@ app_name = 'currency'
 urlpatterns = [
     path('last-rates/', views.LastRates.as_view(), name='last-rates'),
     path('download/rates/', views.RateCSV.as_view(), name='download-rates'),
+    path('latest-rates/', views.LatestRates.as_view(), name='latest-rates'),
 ]
