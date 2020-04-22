871ff67d9e8ff5ba737affab82b947499d387306
ZenCrazyCat <zencrazycat@gmail.com>
2020-04-22 21:41:17 +0300

    added telegram-logging

--- src/account/tasks.py
+++ src/account/tasks.py
@@ -1,3 +1,6 @@
+import requests
+import os
+
 from celery import shared_task
 from django.core.mail import send_mail
 from django.urls import reverse
@@ -22,3 +25,20 @@ def send_activation_code_async(email_to, code):
 @shared_task()
 def send_sms_code_async(phone, code):
     print(phone, code)
+
+
+@shared_task
+def send_tel_message(user):
+	bot_api_key = os.environ['TELEGRAM_BOT_API_KEY']
+	channel_name = '@CurrencyExchangeBotHillel2'
+	message = f'User {user.username} was successfully activated account'
+
+	url = f'https://api.telegram.org/bot{bot_api_key}/sendMessage?chat_id={channel_name}&text={message}'
+
+	params = {
+		'chat_id': channel_name,
+		'text': message
+
+	}
+
+	print(requests.get(url, params=params).json())
